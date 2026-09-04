2c9848bc75905e89097b1e8b50bb7f1361f71e98
mimbot201 <mimbot201@gmail.com>
2026-09-04 22:16:39 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,10 +50,10 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration ---
-TOKEN = os.getenv("BOT_TOKEN", "")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+TOKEN = os.getenv("BOT_TOKEN", "8664260241:AAFfkQD7ErbdMnKZ89gsmcboGgj_uJw6Uv0")
+OWNER_ID = int(os.getenv("OWNER_ID", "6995426618"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", str(OWNER_ID)))
-YOUR_USERNAME = os.getenv("YOUR_USERNAME", "@shiyam7444")
+YOUR_USERNAME = os.getenv("YOUR_USERNAME", "@robotXmehedi")
 UPDATE_CHANNEL = os.getenv("UPDATE_CHANNEL", "https://t.me/shiyam744")
 
 # --- Payment / Hosting Configuration ---
